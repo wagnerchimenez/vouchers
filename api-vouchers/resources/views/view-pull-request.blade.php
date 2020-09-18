2cf8f5219d338a705b716372b003fedf626a586d
Wagner Lima Chimenez <wagnerllchimenez.comp@gmail.com>
2020-09-18 12:01:29 -0400

    Teste de pull-request

---
+++ api-vouchers/resources/views/view-pull-request.blade.php
@@ -0,0 +1,1 @@
+<h3>View teste de pull-request</h3>
--- api-vouchers/routes/web.php
+++ api-vouchers/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste-pull-request', function () {
+    return view('view-pull-request');
+});
